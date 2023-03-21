760bd3979c3ffc039cda8806aac086d4df947d82
Romuald01 <romualdadjimagbe2018@gmail.com>
2023-03-21 01:24:16 +0100

    updating nescafe shift b webpage

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// function openNav() {
+//   document.getElementById("myNav").style.width = "90%";
+//   document.getElementById("myNav").style.height= "100%";
+// }
+
+// function closeNav() {
+//   document.getElementById("myNav").style.width = "0%";
+// }
+// var iconHamburger = document.getElementById("icon-hamburger");
+// if(iconHamburger.click == true) {
+//   document.getElementById("icon-cancel").style.display
+// }
+
+// var a = true;
+// var b= 7;
+// if(a){
+//     console.log("a is equal to true");
+// }else{
+//     console.log("a is equal to false");
+
+// }
+// var x = 1 ; while ( x <= 10 ) { 
+
+//  document.writeln
+//  ("Thesquare of &nbsp;"+ x +"&nbsp;is &nbsp; &nbsp;"+  Math.pow(x,2)+"<br>"); 
+//  x = x + 1;
+// }
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  // code that stop the overflow in y-axis when it is scrolled.
+  document.body.classList.add("no-scroll");
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+function pop() {
+  alert("Your composition will be added to your profile later when registration system is implemented by software engineer Degbe Romuald.");
+}
+
+
